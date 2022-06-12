be2dd3cb9c8b5c28581692864aceda2df3cc09f2
ucheckit7@gmail.com <777788667555887777>
2022-06-13 00:37:19 +0600

    new updated sass and gsap

---
+++ index.js
@@ -0,0 +1,19 @@
+// alert("Hello! I am an alert box!!");
+
+// cards = document.querySelectorAll(".cards");
+
+
+gsap.from("#top-cld", { duration: 2, x: -200, ease: "power2.out" });
+gsap.from("#btm-cld", { duration: 3.5, x: 300, delay: .8, ease: "power3.out" });
+
+
+
+
+
+
+
+
+
+
+
+
